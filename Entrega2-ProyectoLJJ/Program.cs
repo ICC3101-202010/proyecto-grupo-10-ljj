91f8becc63ee714a3e917b5623182a7d41ee7cdd
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-04-24 11:39:22 -0400

    primer commit

---
+++ Entrega2-ProyectoLJJ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entrega2ProyectoLJJ
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
